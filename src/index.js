6a49f88316875fc1fd677e9238552ffc3200a6e3
Kevin Wang <wangkevin448@gmail.com>
2016-10-01 16:01:18 -0700

    scaffold lib

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
-class Node {
-  constructor(op){
-    this._op = op;
+class Tensor {
+  constructor(){
+
   }
 }
 
+class Node {
+  constructor(op, ...parent){
+    this._op = op;
+    this._parent = parent;
+    this._val = null;
+  }
+
+  run(){
+
+  }
+}
+
+class Variable {
+  constructor(){
+
+  }
+}
+
+class State {
+  constructor(){
+
+  }
+}
+
+class Constant {
+  constructor(){
+
+  }
+}
+
+const t = {
+  Variable, State, Constant
+};
+
+const dot = (matrix_1, matrix_2)=>{
+  return matrix_1;
+};
+
+const op = {
+  dot
+};
+
+const run = (node, variables)=>{
+
+};
+
 export {
-  Node
+  t, op, run
 };
